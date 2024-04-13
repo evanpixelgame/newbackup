98cd427d9ba0ec5e33f10b0befb8c1aae3d7d87a
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-13 14:58:59 -0400

    Update baseSceneFunctions.js

--- natquest/js/scenes/baseSceneFunctions.js
+++ natquest/js/scenes/baseSceneFunctions.js
@@ -1,5 +1,5 @@
 export function createKeyboardAssignments(scene) {
-    this.cursors = this.input.keyboard.addKeys({
+    scene.cursors = scene.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
