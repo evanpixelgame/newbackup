f227f3ecdb1f7bfa29541a29c082e296c820d756
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-14 15:36:01 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -91,6 +91,14 @@ export default class OpenWorld extends Phaser.Scene {
         // Create a new instance of the TopIcons class
         this.topIcons = new TopIcons(this, this.sys.game, xIconPositions, yIcons);
 
+        this.uiContainer = this.add.container(0, 0);
+
+    // Add UI elements to the container
+    this.uiContainer.add([topIcons.infoIcon, topIcons.settingsIcon, topIcons.zoomInIcon, topIcons.zoomOutIcon, topIcons.fullscreenIcon]);
+
+    // Optionally, you can set the position of the container relative to the camera
+    this.uiContainer.setScrollFactor(0);
+
     // Add the icons to the scene
 //    topIcons.forEach(icon => {
   //    this.add.existing(icon); // Add each icon to the scene
