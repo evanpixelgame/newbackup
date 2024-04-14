6fb756de3c28fda09dc06dff3533670856107a7b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-14 19:25:38 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -71,8 +71,11 @@ export default class OpenWorld extends Phaser.Scene {
      //creates the animations associated with the user input, ie. 'a' key triggers 'walk-left' animation
      createPlayerAnimations(this);
 
-        // Create a new instance of the TopIcons class
-        this.topIcons = new TopIcons(this, this.game);
+   const uiCamera = this.scene.cameras.add(0, 0, window.innerWidth, window.innerHeight);
+  uiCamera.setScroll(0, 0); // Set UI camera scroll to 0 (fixed position)
+
+  // Create a new instance of the TopIcons class (assuming it accepts the UI camera)
+  this.topIcons = new TopIcons(this, uiCamera);
   }
 
   update(time, delta) {
