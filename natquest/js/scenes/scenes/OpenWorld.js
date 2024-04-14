081dd436b79e738e84b903fa8995bb5f69087d38
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-14 16:38:54 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -77,7 +77,7 @@ export default class OpenWorld extends Phaser.Scene {
      createPlayerAnimations(this);
 
         // Create a new instance of the TopIcons class
-        this.topIcons = new TopIcons(this, this.sys.game, xIconPositions, yIcons);
+        this.topIcons = new TopIcons(this, this.sys.game);
 
   }
 
