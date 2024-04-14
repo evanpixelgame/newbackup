78077271a426a002fecde9902175373771f60982
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-13 23:36:27 -0400

    Update baseSceneFunctions.js

--- natquest/js/scenes/baseSceneFunctions.js
+++ natquest/js/scenes/baseSceneFunctions.js
@@ -38,7 +38,7 @@ export function createMapBoundary(scene, map) {
 
 }
 
-const createCameraConstraints(scene, map, player) {
+export function createCameraConstraints(scene, map, player) {
     scene.cameras.main.setBounds(0, 0, scene.map.widthInPixels, scene.map.heightInPixels);
     scene.cameras.main.startFollow(scene.player, true, 0.05, 0.05);
     scene.cameras.main.setZoom(2);
