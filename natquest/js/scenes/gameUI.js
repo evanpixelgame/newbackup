6b95c618c08e03bb2fab75d21717c8c84d1a010d
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-15 19:06:43 -0400

    Update gameUI.js

--- natquest/js/scenes/gameUI.js
+++ natquest/js/scenes/gameUI.js
@@ -268,7 +268,7 @@ export class TopIcons {
       camera.zoom *= 1.1; // Increase zoom by 10%
       //this.scene.icons.infoIcon.setScale(1.1);
       
-      this.scene.icons.infoIcon.x = 1 * vw / 11; // Set the new X position
+      this.scene.icons.infoIcon.x *= 1.1; // Set the new X position
      // this.scene.icons.infoIcon.y = vh / 11; // Set the new Y position
     } else {
       console.log('Maximum zoom level reached.');
