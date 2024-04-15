c030d142f0f034f2eb13f7a10ae0d5cfce64f271
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-15 17:57:14 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -7,7 +7,7 @@ import { PlayerSprite } from '../PlayerSprite.js';
 import { TopIcons } from '../gameUI.js';
 import { sensorMapSet, createCollisionObjects } from '../collisionHandlers/mapSetter.js';
 import { sensorHandler } from '../collisionHandlers/openWorldCollisionHandler.js';
-import { createMap, createMapBoundary, createCameraConstraints, createKeyboardAssignments, createMobileControls, updatePlayerMovement, createPlayerAnimations, createUIIcons } from '../baseSceneFunctions.js';
+import { createMap, createMapBoundary, createCameraConstraints, createKeyboardAssignments, createMobileControls, updatePlayerMovement, createPlayerAnimations, createUIIcons, handleZoomChange, addZoomChangeListener } from '../baseSceneFunctions.js';
 
 export default class OpenWorld extends Phaser.Scene {
   constructor() {
@@ -82,6 +82,8 @@ export default class OpenWorld extends Phaser.Scene {
 this.icons = createUIIcons(this);
 // Instantiate the gameUI class within the uiLayer, gives functionality to the icons at the top of screen
 this.gameUI = new TopIcons(this, this.game, this.uiLayer, this.icons);
+   //  addZoomChangeListener(this);
+    addZoomChangeListener(this.cameras.main);
   
   }
 
