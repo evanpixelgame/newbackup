16db00caff60155eab885e05eae94986f9df1603
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-14 22:47:17 -0400

    Update baseSceneFunctions.js

--- natquest/js/scenes/baseSceneFunctions.js
+++ natquest/js/scenes/baseSceneFunctions.js
@@ -1,5 +1,5 @@
-export function createMap(scene, mapKey) { //mapkey argument is where you input which map is loaded based on key in preloader scene
-    const map = scene.make.tilemap({ key: mapKey });
+export function createMap(scene, map) { //mapkey argument is where you input which map is loaded based on key in preloader scene
+    const map = scene.make.tilemap({ key: map });
     // Load tileset
     const tilesetsData = [
       { name: 'tilesheetTerrain', key: 'tilesheetTerrain' },
