29b83f931a11236b6f5f2594f1158b49e0c08081
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-15 17:22:40 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -62,10 +62,9 @@ export default class OpenWorld extends Phaser.Scene {
     this.sensorHandling = sensorHandler(this, this.map, this.player);
 
     //Starting configuration for camera, also makes sure camera follow the player
-   // this.cameraConstraints = createCameraConstraints(this, this.map, this.player);
     this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
-    //this.cameras.main.setZoom(2);
+    this.cameras.main.setZoom(2);
     
     //Create mobile or desktop controls for player input, ie. (joystick || keyboard)
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
