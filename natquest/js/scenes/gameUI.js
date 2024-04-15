98b3e4d8975d188b9380439340cfac97fc8b6f0c
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-14 21:20:16 -0400

    Update gameUI.js

--- natquest/js/scenes/gameUI.js
+++ natquest/js/scenes/gameUI.js
@@ -38,9 +38,9 @@ export class TopIcons {
     const icons = {
       infoIcon: this.uiLayer.add(xIconPositions.info, yIcons, 'infoIcon'),
       settingsIcon: this.uiLayer.add(xIconPositions.settings, yIcons, 'settingsIcon'),
-      zoomInIcon: this.uiLayer.create(xIconPositions.zoomIn, yIcons, 'zoomInIcon'),
-      zoomOutIcon: this.uiLayer.add.sprite(xIconPositions.zoomOut, yIcons, 'zoomOutIcon'),
-      fullscreenIcon: this.uiLayer.add.sprite(xIconPositions.fullscreen, yIcons, 'fullscreenIcon'),
+      zoomInIcon: this.uiLayer.add(xIconPositions.zoomIn, yIcons, 'zoomInIcon'),
+      zoomOutIcon: this.uiLayer.add(xIconPositions.zoomOut, yIcons, 'zoomOutIcon'),
+      fullscreenIcon: this.uiLayer.add(xIconPositions.fullscreen, yIcons, 'fullscreenIcon'),
     };
 
     return icons;
