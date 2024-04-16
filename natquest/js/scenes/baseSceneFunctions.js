3a31ab6a0a5eefc9b5b59d7956686afb841a94ef
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-15 23:54:24 -0400

    Update baseSceneFunctions.js

--- natquest/js/scenes/baseSceneFunctions.js
+++ natquest/js/scenes/baseSceneFunctions.js
@@ -223,15 +223,21 @@ export function createPlayerAnimations(scene) { //maybe scene and/or player need
 }
 
 export function createUIIcons(scene) {
-const vw = scene.cameras.main.width;
+    const vw = scene.cameras.main.width;
 
-//const icons = {
-  scene.icons = {
-  infoIcon: scene.add.sprite(1 * vw / 11, 50, 'infoIcon').setInteractive().setScale(0.18).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
-  settingsIcon: scene.add.sprite(6.5 * vw / 9, 50, 'settingsIcon').setInteractive().setScale(0.11).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
-  zoomInIcon: scene.add.sprite(7 * vw / 9, 50, 'zoomInIcon').setInteractive().setScale(0.2).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
-  zoomOutIcon: scene.add.sprite(7.5 * vw / 9, 50, 'zoomOutIcon').setInteractive().setScale(0.2).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
-  fullscreenIcon: scene.add.sprite(8.1 * vw / 9, 50, 'fullscreenIcon').setInteractive().setScale(0.12).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100)
-};
-    return scene.icons;
+    const icons = {
+        infoIcon: scene.add.sprite(100, 50, 'infoIcon').setInteractive().setScale(0.18).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
+        settingsIcon: scene.add.sprite(6.5 * vw / 9, 50, 'settingsIcon').setInteractive().setScale(0.11).setOrigin(0, 0).setScrollFactor(0, 0).setDepth(100),
+        zoomInIcon: scene.add.sprite(7 * vw / 9, 50, 'zoomInIcon').setInteractive().setScale(0.2).setOrigin(0.5, 0).setScrollFactor(0, 0).setDepth(100),
+        zoomOutIcon: scene.add.sprite(7.5 * vw / 9, 50, 'zoomOutIcon').setInteractive().setScale(0.2).setOrigin(0.5, 0).setScrollFactor(0, 0).setDepth(100),
+        fullscreenIcon: scene.add.sprite(8.1 * vw / 9, 50, 'fullscreenIcon').setInteractive().setScale(0.12).setOrigin(0.5, 0).setScrollFactor(0, 0).setDepth(100)
+    };
+
+    // Set ignoreCamera to true for each sprite
+    Object.values(icons).forEach(icon => {
+        icon.ignoreCamera = true;
+    });
+
+    return icons;
 }
+
