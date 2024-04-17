a1e9b04ea432e0ced4db34808ba6c62dd59b2efb
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-16 22:46:11 -0400

    Create NextScene.js

---
+++ natquest/js/scenes/NextScene.js
@@ -0,0 +1,22 @@
+import BaseScene from './BaseScene.js'; // Import your base scene
+
+export default class NextScene extends BaseScene {
+  constructor() {
+    super({ key: 'CustomScene' });
+        this.mapKey = 'insideroom';
+    // Add any additional properties specific to CustomScene
+  }
+
+  // Override any methods from BaseScene as needed
+
+  // Add any additional methods specific to CustomScene
+
+  create() {
+    // Call the create method of the base scene
+    super.create();
+
+    // Add any additional creation logic specific to CustomScene
+  }
+
+  // Add any other lifecycle methods as needed
+}
