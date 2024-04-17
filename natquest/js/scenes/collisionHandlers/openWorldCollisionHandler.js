7855a3bbf9db0829e7226dede3f259b7d1e646ec
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-16 22:54:08 -0400

    Update openWorldCollisionHandler.js

--- natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
+++ natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
@@ -4,7 +4,7 @@
 import NewScene from '../scenes/NewScene.js';
 import OpenWorld from '../scenes/OpenWorld.js';
 import BaseScene from '../BaseScene.js';
-import NewSceneTest from '../scenes/NewSceneTest.js';
+import NewSceneTest from '../NewSceneTest.js';
 import PlayerControls from '../PlayerControls.js';
 
 export function sensorHandler(scene, map, player, transitionSensors) {
