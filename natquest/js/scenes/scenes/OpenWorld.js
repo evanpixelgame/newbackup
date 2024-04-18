4b6a72d75c99dddecf035ddfcc243092a7bb74bd
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-17 21:39:44 -0400

    Create OpenWorld.js

---
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -0,0 +1,33 @@
+import BaseScene from '../BaseScene.js';
+
+ export default class NewScene extends BaseScene {
+  constructor() {
+   super('NewScene');
+  }
+
+  init(data) {
+    // Initialize scene properties from the data passed from the previous scene
+    super.init();
+    this.mapKey = 'map';
+  //  this.player = data.player || null;
+    this.velocityChange = 2;
+    this.startPosX = 495;
+    this.startPosY = 325;
+    this.playerPosX = 495;
+    this.playerPosY = 325;
+    this.cameraZoomLevel = 2;
+    console.log('Player received in OpenWorld:', this.player);
+  }
+
+  preload() {
+  }
+
+  create() {
+   super.create();
+  }
+
+  update(time, delta) {
+   super.update(time, delta);
+    // Update logic for the scene, if necessary
+  }
+}
