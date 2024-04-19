a2ac164436bd2ea78f903c440e7fbd24afc8c860
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-19 19:34:26 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -1,100 +1,33 @@
-import { PlayerSprite } from '../PlayerSprite.js';
-import NewScene from './NewScene.js';
-import { TopIcons } from '../gameUI.js';
-import { sensorMapSet, createCollisionObjects } from '../collisionHandlers/mapSetter.js';
-import { sensorHandler } from '../collisionHandlers/openWorldCollisionHandler.js';
-import { createMap, createMapBoundary, createCameraConstraints, createKeyboardAssignments, createMobileControls, updatePlayerMovement, createPlayerAnimations, createUIIcons } from '../baseSceneFunctions.js';
+import BaseScene from '../BaseScene.js';
 
-export default class OpenWorld extends Phaser.Scene {
+ export default class OpenWorld extends BaseScene {
   constructor() {
-    super({ key: 'OpenWorld' });
-
-    this.engine = null;
-    this.world = null;
-    this.map = null;
-    this.mapKey = null;
-    this.player = null;
-    this.startPosX = null;
-    this.startPosY = null;
-    this.velocityChange = null;
-    this.cameraZoomLevel = 2;
-    this.topIcons = null;
+   super('OpenWorld');
   }
 
   init(data) {
-  //  this.openWorldScene = data.OpenWorld;
-    this.mapKey = data.mapKey || 'map';
-    this.player = data.player;
-    this.velocityChange = data.velocityChange || 2;
-    this.startPosX = data.startPosX || 495;
-    this.startPosY = data.startPosY || 325;
-    this.playerPosX = data.playerPosX || 495;
-    this.playerPosY = data.playerPosY || 325;
-    this.cameraZoomLevel = data.cameraZoomLevel || this.cameraZoomLevel;
-    this.topIcons = data.topIcons;
+    // Initialize scene properties from the data passed from the previous scene
+   // super.init();
+    this.mapKey = 'map';
+    //this.player = data.player;
+    this.velocityChange = 2;
+    this.startPosX = 495;
+    this.startPosY = 325;
+    this.playerPosX = 495;
+     this.playerPosY = 325;
+    this.cameraZoomLevel = 2;
+    console.log('Player received in NextRoom:', this.player);
+  }
+
+  preload() {
   }
 
   create() {
-    
-    // Create Matter.js engine
-    this.matterEngine = Phaser.Physics.Matter.Matter.World;
-    this.engine = this.matter.world;
-    this.world = this.matterEngine.create({
-      // your Matter.js world options here
-    });
-
-    this.icons = createUIIcons(this);
-    
-    //Creates the scene's map from Tiled JSON data
-    this.map = createMap(this, this.mapKey);
-
-    //Creates a new instance of the PlayerSprite class to add a matter.js player body object to the scene
-    this.player = new PlayerSprite(this, this.startPosX, this.startPosY, 'player');
-
-    //Creates a boundary around outer border of map so player cannot move outside the visible map
-    this.worldBounds = createMapBoundary(this, this.map, this.world);
-
-    //Takes the scene's map and creates the barriers where the player cannot pass through from the map's Collision Layer
-    this.collisionObjects = createCollisionObjects(this, this.map);
-    
-    //Takes the scene's map and creates sensor objects based on the map's Sensor Layer
-    this.sensorMapping = sensorMapSet(this, this.map, this.sensorID);
-    
-    //Creates switch cases with event listeners for what should happen when sensors ojjects are triggered in this scene/map, each scene may need its own unique sensorHandler
-   this.sensorHandling = sensorHandler(this, this.map, this.player);
-
-    //Starting configuration for camera, also makes sure camera follow the player
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
-    this.cameras.main.setZoom(this.cameraZoomLevel);
-    
-    //Create mobile or desktop controls for player input, ie. (joystick || keyboard)
-    if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
-    createMobileConrols(this); } else { createKeyboardAssignments(this); }
-    
-     //creates the animations associated with the user input, ie. 'a' key triggers 'walk-left' animation
-     createPlayerAnimations(this);
-
-    //creates the UI icons and graphics and make up the game's UI/HUD
-    this.icons = createUIIcons(this);
-    
-    // Instantiate the gameUI class within the uiLayer, gives functionality to the icons at the top of screen
-    this.gameUI = new TopIcons(this, this.game, this.uiLayer, this.icons);
-
-/*
-    // Add a delay before starting a new scene
-const delayMilliseconds = 2000; // Adjust this value to set the delay time (in milliseconds)
-setTimeout(() => {
-    this.scene.add('NewScene', NewScene);
-    this.scene.start('NewScene'); // 'newSceneKey' is a unique key for the scene, NewScene is the constructor function for the new scene, and true indicates auto-start
-}, delayMilliseconds);
-*/
-
+   super.create();
   }
 
   update(time, delta) {
-    //Update the position of player based on user input and velocity
-    updatePlayerMovement(this, this.player, this.velocityChange); 
+   super.update(time, delta);
+    // Update logic for the scene, if necessary
   }
-  
 }
