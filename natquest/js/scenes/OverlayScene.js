c9c56ab8fd8be48cbfd757d96cec3ec2ebd4baab
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 17:12:29 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -1,1 +1,24 @@
+export class OverlayScene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'OverlayScene'
+    })
+  }
+
+  init(data) {
+    this.player = data.player;
+  }
+
+  preload() {
+
+  }
+
+  create() {
+  }
+  
+  update() {
+
+  }
+}
+
 
