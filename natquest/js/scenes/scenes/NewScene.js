ae7388630db2a059c56f06341612c232c1594162
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 01:38:53 -0400

    Update NewScene.js

--- natquest/js/scenes/scenes/NewScene.js
+++ natquest/js/scenes/scenes/NewScene.js
@@ -26,8 +26,8 @@ import { sensorHandler } from '../collisionHandlers/newSceneCollisionHandler.js'
   create() {
    super.create();
    this.sensorHandling = sensorHandler(this, this.map, this.player);
-  console.log(this.map);
-   this.map.gameObject.setScale(2, 2);
+  console.log(this.map.scaleX, this.map.scaleY);
+ //  this.map.gameObject.setScale(2, 2);
   // this.map.setDisplaySize(2, 2);
    this.player.setScale(2);
   }
