63939a0aa9f9aa762397ad230bc040c72498ee7b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 17:15:42 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -5,7 +5,7 @@ export class OverlayScene extends Phaser.Scene {
   }
 
   init(data) {
-    this.player = data.player;
+   // this.player = data.player;
   }
 
   preload() {
@@ -13,6 +13,17 @@ export class OverlayScene extends Phaser.Scene {
   }
 
   create() {
+    
+    this.healthBar = this.add.rectangle(
+    (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
+    (window.innerHeight / 4) / 2 / 2,  // Y coordinate relative to the viewport
+    50,  // Width of the object
+    300,  // Height of the object
+    0xff0000  // Color of the object (red)
+);
+    this.healthBar.setScrollFactor(0, 0);
+    this.healthBar.setDepth(1000);
+    
   }
   
   update() {
