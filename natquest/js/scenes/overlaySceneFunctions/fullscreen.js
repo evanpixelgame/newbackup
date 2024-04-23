7edc78a8a55ea97fec23043d389b72af686a3f30
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 20:07:16 -0400

    Update fullscreen.js

--- natquest/js/scenes/overlaySceneFunctions/fullscreen.js
+++ natquest/js/scenes/overlaySceneFunctions/fullscreen.js
@@ -1,5 +1,5 @@
 export function createFullscreenIcon(scene) {
-
+const vw = window.innerWidth;
 const fullscreenIcon = scene.add.sprite(8.1 * vw/ 9, 50, 'fullscreenIcon').setInteractive().setScale(.12);
 
     fullscreenIcon.on('pointerdown', () => {
