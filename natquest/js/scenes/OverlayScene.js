2b5deafa4913f5c926f0023c14fc6718d9b875c9
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 22:48:15 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -18,18 +18,18 @@ export default class OverlayScene extends Phaser.Scene {
   }
 
   create() {
+
+//add a function that determines the active scene and set up event listener so that it updates it when active scene changes
+    console.log('this.scene.manager from Overlay Scene: ' + this.scene.manager);
+    console.log('this.scene.manager.scenes from Overlay Scene: ' + this.scene.manager.scenes);
+    
     this.fullscreenIcon = createFullscreenIcon(this); //fullscreen icon, positioned in top right corner of viewport
 
     this.zoomIcons = createZoomIcons(this); // positioned directly to left of fullscreen icon, at about 3/4 viewport 
-
-    console.log('this.scene.manager from Overlay Scene: ' + this.scene.manager);
-    console.log('this.scene.manager.scenes from Overlay Scene: ' + this.scene.manager.scenes);
-
     
     this.healthBar = createHealthBar(this);
 
-
-this.resizer = setupResizeListener(this);
+    this.resizer = setupResizeListener(this);
     
   }
 
