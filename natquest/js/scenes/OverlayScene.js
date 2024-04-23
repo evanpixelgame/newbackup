100ac714944d2e823f22911eeccb478c5a26575b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 20:43:40 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -1,4 +1,5 @@
 import { createFullscreenIcon } from './overlaySceneFunctions/fullscreen.js';
+// import { createZoomIcons } from './overlaySceneFunctions/zoom.js';
 
 export default class OverlayScene extends Phaser.Scene {
   constructor() {
@@ -15,7 +16,13 @@ export default class OverlayScene extends Phaser.Scene {
   }
 
   create() {
-    this.fullscreenIcon = createFullscreenIcon(this);
+    this.fullscreenIcon = createFullscreenIcon(this); //fullscreen icon, positioned in top right corner of viewport
+
+//    this.zoomIcons = createZoomIcons(this); // positioned directly to left of fullscreen icon, at about 3/4 viewport 
+
+    console.log('this.scene.manager from Overlay Scene: ' + this.scene.manager);
+    console.log('this.scene.manager.scenes from Overlay Scene: ' + this.scene.manager.scenes);
+
     
     this.healthBar = this.add.rectangle(
     (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
