002646671372ed9ab35dd767d4def2c5a8f45c3f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 22:23:57 -0400

    Update resizer.js

--- natquest/js/scenes/overlaySceneFunctions/resizer.js
+++ natquest/js/scenes/overlaySceneFunctions/resizer.js
@@ -1,15 +1,15 @@
-export function resizeGame(gameSize) {
+export function resizeGame(scene, gameSize) {
       
         const { width, height } = gameSize;
 
         // Resize the game canvas
-        this.sys.game.canvas.style.width = width + 'px';
-        this.sys.game.canvas.style.height = height + 'px';
+        scene.sys.game.canvas.style.width = width + 'px';
+        scene.sys.game.canvas.style.height = height + 'px';
 
         // Resize the game config to match the new size
-        this.sys.game.config.width = width;
-        this.sys.game.config.height = height;
+        scene.sys.game.config.width = width;
+        scene.sys.game.config.height = height;
 
         // Call resize events on all scenes
-        this.events.emit('resize', gameSize);
+        scene.events.emit('resize', gameSize);
     }
