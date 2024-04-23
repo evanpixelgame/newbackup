6bf7aef1dc3ccaa11753b6491287da07eeb4a9ac
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-23 00:52:02 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -35,11 +35,11 @@ export default class OverlayScene extends Phaser.Scene {
     console.log(scene.scene.key); // Access the key of each scene
     console.log(scene); // Log each scene object
     if (scene.scene.key !== 'OverlayScene') {
-    this.activeScene = scene.scene;
+    this.activeScene = scene;
 }
 });
 
-    console.log('titi is the prettiest in universe and here is only active under scene: ' + this.activeScene + this.activeScene.key);
+    console.log('titi is the prettiest in universe and here is only active under scene: ' + this.activeScene.scene.key + this.activeScene);
 
     
   }
