6f1d3383029e69d4186d98310566fb50d92eccd8
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 23:00:27 -0400

    Update OverlayScene.js

--- natquest/js/scenes/OverlayScene.js
+++ natquest/js/scenes/OverlayScene.js
@@ -1,5 +1,5 @@
 import { createFullscreenIcon } from './overlaySceneFunctions/fullscreen.js';
-import { createZoomIcons } from './overlaySceneFunctions/zoom.js';
+//import { createZoomIcons } from './overlaySceneFunctions/zoom.js';
 import { resizeGame, setupResizeListener } from './overlaySceneFunctions/resizer.js';
 import { createHealthBar } from './overlaySceneFunctions/healthBar.js';
 
@@ -18,15 +18,16 @@ export default class OverlayScene extends Phaser.Scene {
   }
 
   create() {
-    this.activeScene = 'gottafigureout';
+//    this.activeScene = 'gottafigureout';
 
 //add a function that determines the active scene and set up event listener so that it updates it when active scene changes
     console.log('this.scene.manager from Overlay Scene: ' + this.scene.manager);
     console.log('this.scene.manager.scenes from Overlay Scene: ' + this.scene.manager.scenes);
+     console.log('this.scene.manager.scenes.scene from Overlay Scene: ' + this.scene.manager.scenes.scene);
     
     this.fullscreenIcon = createFullscreenIcon(this); //fullscreen icon, positioned in top right corner of viewport
 
-    this.zoomIcons = createZoomIcons(this, this.activeScene); // positioned directly to left of fullscreen icon, at about 3/4 viewport 
+   // this.zoomIcons = createZoomIcons(this, this.activeScene); // positioned directly to left of fullscreen icon, at about 3/4 viewport 
     
     this.healthBar = createHealthBar(this);
 
