b9bd1611a22ff7cc20586ce921fee1a6db2f4837
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-23 03:32:30 -0400

    Update zoom.js

--- natquest/js/scenes/overlaySceneFunctions/zoom.js
+++ natquest/js/scenes/overlaySceneFunctions/zoom.js
@@ -5,7 +5,14 @@ export function createZoomIcons(scene, activeScene) {
    const zoomInIcon = scene.add.sprite(7 * vw / 9, 50, 'zoomInIcon').setInteractive().setScale(0.2);
    const zoomOutIcon = scene.add.sprite(7.5 * vw / 9, 50, 'zoomOutIcon').setInteractive().setScale(0.2);
 
- scene.events.on('activeSceneChanged', scene.updateActiveScene, this);
+// scene.events.on('activeSceneChanged', scene.updateActiveScene, this);
+
+    scene.events.on('newActiveScene', (newScene) => {
+        // Update the activeScene variable
+        activeScene = newScene;
+        // Perform any other actions based on the scene change
+        // For example, update zoom based on the new active scene
+    });
   
  zoomInIcon.on('pointerdown', () => {
          if (activeScene.cameras.main.zoom < 3) {
