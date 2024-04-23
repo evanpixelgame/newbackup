647fbd05d713248d3a5f9a5a897934370df5e010
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-22 20:43:48 -0400

    Create zoom.js

---
+++ natquest/js/scenes/overlaySceneFunctions/zoom.js
@@ -0,0 +1,32 @@
+  
+export function createZoomIcon(scene) {
+  
+   const zoomInIcon = scene.add.sprite(7 * vw / 9, 50, 'zoomInIcon').setInteractive().setScale(0.2);
+   const zoomOutIcon = scene.add.sprite(7.5 * vw / 9, 50, 'zoomOutIcon').setInteractive().setScale(0.2);
+
+ zoomInIcon.on('pointerdown', () => {
+        zoomIn();
+        });
+
+ zoomOutIcon.on('pointerdown', () => {
+        zoomOut();
+        });
+  
+  return 
+}
+
+function zoomIn(scene) {
+  if (scene.cameras.main.zoom < 3) {
+    scene.cameras.main.zoom *= 1.1; // Increase zoom by 10%
+  } else {
+    console.log('Maximum zoom level reached.');
+  }
+}
+
+function zoomOut(scene) {
+  if (scene.cameras.main.zoom > 1) { // Set a minimum zoom level (0.2 is just an example)
+    scene.cameras.main.zoom /= 1.1; // Decrease zoom by 10%
+  } else {
+    console.log('Minimum zoom level reached.');
+  }
+}
