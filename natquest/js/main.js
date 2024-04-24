98dc283cfda8d7340a56e569c08762b1ad085eac
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-23 23:14:30 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -1,12 +1,12 @@
 import { Preloader } from "./scenes/gameStartScenes/Preloader.js";
-import CustomEmitter from './scenes/CustomEmitter.js';
+//import CustomEmitter from './scenes/CustomEmitter.js';
 
 //set the width and height of the canvas equal the width and height of the screen it's being played on
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const customEmitter = new CustomEmitter();
-    
+const customEmitter = new Phaser.Events.EventEmitter();   
+
 const config = {
   type: Phaser.AUTO,
   width: width,
