97ea2533344da4e24a9ea78b9f371b97cb3746e1
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-23 21:35:58 -0400

    Create EventEmitter.js

---
+++ natquest/js/scenes/EventEmitter.js
@@ -0,0 +1,23 @@
+// Define a custom event emitter class
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    // Method to subscribe to events
+    on(eventName, listener) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+        this.events[eventName].push(listener);
+    }
+
+    // Method to emit events
+    emit(eventName, ...args) {
+        if (this.events[eventName]) {
+            this.events[eventName].forEach(listener => {
+                listener(...args);
+            });
+        }
+    }
+}
