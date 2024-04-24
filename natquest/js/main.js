8f1c468c677db905386d6ad381c5774f466da898
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-24 16:43:59 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -5,26 +5,26 @@ const customEmitter = new CustomEmitter();
 export default customEmitter;
 
 const config = {
-  type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: '#FDD5D5', //could also be replaced with image
-  parent: 'game-container',
-  pixelArt: true,
-  scale: {
-    mode: Phaser.Scale.RESIZE, //this just makes it so that the game canvas always tries to automatically adjust so that it fills the available screen
-  },
-  physics: {
-    default: "matter",
-    matter: {
-      gravity: { y: 0 }, 
-      debug: true,
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: '#FDD5D5', //could also be replaced with image
+    parent: 'game-container',
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.RESIZE, //this just makes it so that the game canvas always tries to automatically adjust so that it fills the available screen
     },
-  },
- scene: [
-    Preloader,
-  ],
-  interpolation: true,
+    physics: {
+        default: "matter",
+        matter: {
+            gravity: { y: 0 },
+            debug: true,
+        },
+    },
+    scene: [
+        Preloader,
+    ],
+    interpolation: true,
 };
 
 const game = new Phaser.Game(config);
