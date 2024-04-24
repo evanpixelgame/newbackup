16d55906bc033dc541424285df5c09f7a81db966
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-23 20:51:17 -0400

    Update openWorldCollisionHandler.js

--- natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
+++ natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
@@ -37,7 +37,8 @@ export function sensorHandler(scene, map, player, transitionSensors) {
         scene.scene.bringToTop('NewScene'); 
       scene.scene.bringToTop('OverlayScene');
      // OverlayScene.handleSceneChange();
-      overlayScene.customEmit('activeSceneChanged');  //if this works, add bring to top to the active scene change method
+      //overlayScene.customEmit('activeSceneChanged');  //if this works, add bring to top to the active scene change method
+       scene.scene.get('OverlayScene').customEmit('activeSceneChanged');
     } else {
       console.log('youve hit the door sensor for the first time');
       console.log('x position: ' + scene.player.x + '  y position: ' + scene.player.y);
@@ -57,7 +58,7 @@ export function sensorHandler(scene, map, player, transitionSensors) {
       // OverlayScene.handleSceneChange();
      // scene.eventEmitter = new Phaser.Events.EventEmitter();
     // overlayScene.customEmit('activeSceneChanged'); //if this works, add bring to top to the active scene change method
-    scene.get('OverlayScene').customEmit('activeSceneChanged');
+    scene.scene.get('OverlayScene').customEmit('activeSceneChanged');
     }
     break;
               
