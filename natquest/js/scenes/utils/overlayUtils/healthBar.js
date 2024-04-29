2dbe49d61ff5df0f5ca567fcd297b8fa8c406a46
evanpixelgame <evanpixelgame@gmail.com>
2024-04-29 19:58:17 -0400

    #healthbarpushess

--- natquest/js/scenes/utils/overlayUtils/healthBar.js
+++ natquest/js/scenes/utils/overlayUtils/healthBar.js
@@ -1,12 +1,36 @@
 export function createHealthBar(scene) {
   
-scene.healthBar = scene.add.rectangle(
+
+    scene.healthBarBackground = scene.add.rectangle(
+    ((window.innerWidth / 4) / 2 / 2) - 5,  // X coordinate relative to the viewport
+        ((window.innerHeight / 4) / 2 / 2),  // Y coordinate relative to the viewport
+        60,  // Width of the object
+        320,  // Height of the object
+        0x000000  // Color of the object
+    );
+        scene.healthBar.setScrollFactor(0, 0);
+        scene.healthBar.setDepth(100000);
+
+    
+
+    scene.healthBar = scene.add.rectangle(
     (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
     (window.innerHeight / 4) / 2 / 2,  // Y coordinate relative to the viewport
     50,  // Width of the object
-    300,  // Height of the object
+    10,  // Height of the object
     0xff0000  // Color of the object (red)
 );
     scene.healthBar.setScrollFactor(0, 0);
-    scene.healthBar.setDepth(100000);
+    scene.healthBar.setDepth(100001);
+
+    scene.healthBarDepletion = scene.add.rectangle(
+        (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
+        (window.innerHeight / 4) / 2 / 2,  // Y coordinate relative to the viewport
+        50,  // Width of the object
+        300,  // Height of the object
+        0xffffff  // Color of the object (red)
+    );
+        scene.healthBar.setScrollFactor(0, 0);
+        scene.healthBar.setDepth(100002);
+
 }
