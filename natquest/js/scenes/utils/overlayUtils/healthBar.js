3c562cf05fdf64c72254945667d4fc641d7edff9
evanpixelgame <evanpixelgame@gmail.com>
2024-04-29 20:38:51 -0400

    healthbarreadjusted

--- natquest/js/scenes/utils/overlayUtils/healthBar.js
+++ natquest/js/scenes/utils/overlayUtils/healthBar.js
@@ -1,36 +1,39 @@
 export function createHealthBar(scene) {
-  
+
 
     scene.healthBarBackground = scene.add.rectangle(
-    ((window.innerWidth / 4) / 2 / 2) - 5,  // X coordinate relative to the viewport
-        ((window.innerHeight / 4) / 2 / 2),  // Y coordinate relative to the viewport
-        60,  // Width of the object
-        320,  // Height of the object
+        10,  // X coordinate relative to the viewport
+        5,  // Y coordinate relative to the viewport
+        70,  // Width of the object
+        310,  // Height of the object
         0x000000  // Color of the object
     );
-        scene.healthBarBackground.setScrollFactor(0, 0);
-        scene.healthBarBackground.setDepth(100000);
+    scene.healthBarBackground.setScrollFactor(0, 0);
+    scene.healthBarBackground.setDepth(100000);
+    scene.healthBarBackground.setOrigin(0, 0);
 
-    
+
 
     scene.healthBar = scene.add.rectangle(
-    (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
-    (window.innerHeight / 4) / 2 / 2,  // Y coordinate relative to the viewport
-    50,  // Width of the object
-    10,  // Height of the object
-    0xff0000  // Color of the object (red)
-);
-    scene.healthBar.setScrollFactor(0, 0);
-    scene.healthBar.setDepth(100001);
-
-    scene.healthBarDepletion = scene.add.rectangle(
-        (window.innerWidth / 4) / 2 / 2,  // X coordinate relative to the viewport
-        (window.innerHeight / 4) / 2 / 2,  // Y coordinate relative to the viewport
+        20,  // X coordinate relative to the viewport
+        5,  // Y coordinate relative to the viewport
         50,  // Width of the object
         300,  // Height of the object
+        0xff0000  // Color of the object (red)
+    );
+    scene.healthBar.setScrollFactor(0, 0);
+    scene.healthBar.setDepth(100001);
+    scene.healthBar.setOrigin(0, 0);
+
+    scene.healthBarDepletion = scene.add.rectangle(
+        20,  // X coordinate relative to the viewport
+        5,  // Y coordinate relative to the viewport
+        50,  // Width of the object
+        10,  // Height of the object
         0xffffff  // Color of the object (red)
     );
-        scene.healthBarDepletion.setScrollFactor(0, 0);
-        scene.healthBarDepletion.setDepth(100002);
+    scene.healthBarDepletion.setScrollFactor(0, 0);
+    scene.healthBarDepletion.setDepth(100002);
+    scene.healthBarDepletion.setOrigin(0, 0);
 
 }
